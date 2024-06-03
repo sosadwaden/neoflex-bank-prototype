59234988eadb60e52fbb3b7230a381bfd88ebfe0
sosadwaden <wadenjap@gmail.com>
2024-06-03 03:50:06 +0300

    Initial commit

---
+++ src/test/java/com/sosadwaden/neoflexbankprototype/NeoflexBankPrototypeApplicationTests.java
@@ -0,0 +1,13 @@
+package com.sosadwaden.neoflexbankprototype;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class NeoflexBankPrototypeApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
